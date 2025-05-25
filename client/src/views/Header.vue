8b8eaec3468cd9dfc0d31ed1a65df046aca93bb7
Kunal-Sharma-8650 <mr.kunnugaur@gmail.com>
2025-05-25 13:08:31 +0530

    first program

---
+++ client/src/views/Header.vue
@@ -0,0 +1,8 @@
+<template>
+    <h3>
+        Shreyas Iyer's mental state 'unimaginable' after England snub: 'He thought this was the time to settle in Test team
+    </h3>
+    <p>
+        The Board of Control for Cricket in India (BCCI) on Saturday unveiled Team India's 18-member squad for the upcoming Test series against England, which will commence on 20 June. The retirement of stalwarts Rohit Sharma and Virat Kohli from Test cricket has paved the way for a new generation of talent to step into the spotlight, with several young players earning their spots in the national side. Among those expected to make a return to the Test team was Shreyas Iyer, a batter renowned for his consistency at both domestic and international levels. Despite his impressive performances over the past year, Iyer was surprisingly omitted from the squad, leaving a whole lot of people surprised.
+    </p>
+</template>
